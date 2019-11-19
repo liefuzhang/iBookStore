309c912f9285a08a586f58ef86af01fda44fc727
liefu zhang <liefuzhangnz@gmail.com>
2019-11-19 20:10:56 +1300

    Adding catalog management

---
+++ Web/iBookStoreMVC/Controllers/CatalogManagementController.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using iBookStoreMVC.Service;
+using iBookStoreMVC.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace iBookStoreMVC.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class CatalogManagementController : Controller
+    {
+        private readonly ICatalogService _catalogService;
+
+        public CatalogManagementController(ICatalogService catalogSvc)
+        {
+            _catalogService = catalogSvc;
+        }
+
+        public async Task<IActionResult> Index(int? page)
+        {
+            var itemsPerPage = 10;
+            var vm = await _catalogService.GetCatalogItems(page ?? 0, itemsPerPage, null);
+            return View(vm);
+        }
+    }
+}
