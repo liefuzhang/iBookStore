9c2118e598f7726f113bc39e421a6fc27664154a
Liefu Zhang <Liefu.Zhang@vocusgroup.co.nz>
2020-10-11 15:32:59 +1300

    Added newsletter service and unit tests

---
+++ Services/UserManagement/UserManagement.API/Service/NewsletterService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Text.RegularExpressions;
+
+namespace UserManagement.API.Service
+{
+    public class NewsletterService : INewsletterService
+    {
+        private readonly ConcurrentBag<string> _emailBag = new ConcurrentBag<string>();
+
+        public void AddEmailToBag(string email)
+        {
+            if (string.IsNullOrWhiteSpace(email) || !IsValidEmail(email))
+                throw new ArgumentException(nameof(email));
+
+            _emailBag.Add(email);
+        }
+
+        private bool IsValidEmail(string email)
+        {
+            return Regex.IsMatch(email,
+                @"^[^@\s]+@[^@\s]+\.[^@\s]+$",
+                RegexOptions.IgnoreCase);
+        }
+    }
+}
---
+++ Services/UserManagement/UserManagement.UnitTests/Service/NewsletterServiceTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using NUnit.Framework;
+using UserManagement.API.Service;
+
+namespace UserManagement.UnitTests.Service
+{
+    [TestFixture]
+    public class NewsletterServiceTests
+    {
+        [Test]
+        [TestCase("")]
+        [TestCase(null)]
+        [TestCase("invalid_email")]
+        [TestCase("dd@dd")]
+        [TestCase("dd.dd@dd")]
+        [TestCase("dd@dd@dd")]
+        public void AddEmailToBag_ThrowsException_WhenEmailInvalid(string email)
+        {
+            var service = new NewsletterService();
+
+            Assert.Throws<ArgumentException>(() => service.AddEmailToBag(email));
+        }
+
+        [Test]
+        [TestCase("valid_email@dd.dd")]
+        [TestCase("valid_email@dd.dd.dd")]
+        [TestCase("dd.dd@dd.dd")]
+        public void AddEmailToBag_NotThrowException_WhenEmailValid(string email)
+        {
+            var service = new NewsletterService();
+
+            service.AddEmailToBag(email);
+        }
+    }
+}
