47d5b766f836ddd39a78dc7f19cb3cfb9d5a3c9b
Liefu Zhang <Liefu.Zhang@vocusgroup.co.nz>
2020-11-29 14:20:29 +1300

    Fix postgres issue

--- Services/Ordering/Ordering.API/Application/Queries/OrderQueries.cs
+++ Services/Ordering/Ordering.API/Application/Queries/OrderQueries.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
+using Npgsql;
 
 namespace Ordering.API.Application.Queries
 {
@@ -17,16 +18,16 @@ namespace Ordering.API.Application.Queries
         }
 
         public async Task<IEnumerable<OrderSummary>> GetOrdersForUserAsync(string userId) {
-            using (var connection = new SqlConnection(_connectionString)) {
+            using (var connection = new NpgsqlConnection(_connectionString)) {
                 connection.Open();
 
-                return await connection.QueryAsync<OrderSummary>(@"SELECT o.[Id] as OrderNumber,o.[CreatedDate] as [CreatedDate],o.[Status] as [Status], SUM(oi.units*ROUND(oi.unitprice*o.currencyRate, 2)) as Total, o.[Currency] as [Currency]
-                     FROM [dbo].[Orders] o
-                     LEFT JOIN[dbo].[orderitems] oi ON  o.Id = oi.orderid                   
-                     LEFT JOIN[dbo].[buyers] ob on o.BuyerId = ob.Id
-                     WHERE ob.IdentityGuid = @userId
-                     GROUP BY o.[Id], o.[CreatedDate], o.[Status], o.[Currency]
-                     ORDER BY o.[Id]", new { userId });
+                return await connection.QueryAsync<OrderSummary>($@"SELECT o.""Id"" as OrderNumber,o.""CreatedDate"" as CreatedDate,o.""Status"" as Status, SUM(oi.""Units""*ROUND(oi.""UnitPrice""*o.""CurrencyRate"", 2)) as Total, o.""Currency"" as Currency
+                     FROM ""Orders"" o
+                     LEFT JOIN ""OrderItems"" oi ON  o.""Id"" = oi.""OrderId""                   
+                     LEFT JOIN ""Buyers"" ob on o.""BuyerId"" = ob.""Id""
+                     WHERE ob.""IdentityGuid"" = '{userId}'
+                     GROUP BY o.""Id"", o.""CreatedDate"", o.""Status"", o.""Currency""
+                     ORDER BY o.""Id""");
             }
         }
     }
